6543c246387762149db2db439ff644faa562a440
vlaghe <vlaghe@protonmail.com>
2022-07-21 17:33:52 +0300

    added nvim keybinds for compiling docs & preview

--- .config/nvim/lua/custom/mappings.lua
+++ .config/nvim/lua/custom/mappings.lua
@@ -1,4 +1,10 @@
 local M = {}
 
+M.latex = {
+  n = {
+    ["<leader>cc"] = {":w! | !compiler '%' <CR>", "Compile doc (groff, LaTeX, markdown, etc)"},
+    ["<leader>pp"] = {":!opout '%' <CR><CR>", "Open corresponding .pdf/.html or preview"}
+  }
+}
 
 return M
